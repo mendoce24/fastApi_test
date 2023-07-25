aa062950855844a4a0b3eb034eef6315056e7cf4
Cesar Mendoza <52425885+mendoce24@users.noreply.github.com>
2023-07-25 16:59:41 +0200

    implementing HTTPException

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import json
 
 app = FastAPI()
@@ -17,10 +17,10 @@ def update_item(item: dict):
 
     if len(item) < 3:
         FIELD_THE_USER_FORGOT_HERE = "salary" if ('salary' not in item.keys()) else "bonus" if ('bonus' not in item.keys()) else "taxes"
-        return f"3 fields expected (salary, bonus, taxes). You forgot: {FIELD_THE_USER_FORGOT_HERE}."
+        raise HTTPException(f"3 fields expected (salary, bonus, taxes). You forgot: {FIELD_THE_USER_FORGOT_HERE}.")
     elif (str(item['salary']).isdigit() & 
           str(item['bonus']).isdigit() &
           str(item['taxes']).isdigit()) == False:
-        return 'expected numbers, got strings.'
+        raise HTTPException('expected numbers, got strings.')
   
     return (item['salary'] + item['bonus'] - item['taxes']) # salary + bonus - taxes
